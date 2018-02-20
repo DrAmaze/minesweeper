9e5ac4d04df0c70c7cc2e23a020b1223b80c3335
jacob <jbutler8@terpmail.umd.edu>
2018-02-20 07:40:50 -0800

    complete board class

--- board.rb
+++ board.rb
@@ -1,12 +1,62 @@
 require_relative 'tile'
 
 class Board
+  attr_reader :grid_size, :num_bombs
+
   def initialize(num_bombs = 10, grid_size = 9)
     @num_bombs, @grid_size = num_bombs, grid_size
-    populate_bombs(num_bombs)
+
+    generate_board
   end
 
-  def populate_bombs(n)
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
 
+  def render(reveal = false)
+    @grid.map do |row|
+      row.map do |tile|
+        reveal ? tile.reveal : tile.render
+      end.join("")
+    end.join("\n")
+  end
+
+  def reveal
+    render(true)
+  end
+
+  def lost?
+    @grid.flatten.any? { |tile| tile.bombed? && tile.visited? }
+  end
+
+  def won?
+    @grid.flatten.all? { |tile| tile.bombed? != tile.visited }
+  end
+
+  private
+
+  def generate_board
+    def generate_board
+    @grid = Array.new(@grid_size) do |row|
+      Array.new(@grid_size) { |col| Tile.new(self, [row, col]) }
+    end
+
+    plant_bombs
+  end
+
+  def plant_bombs
+    planted = 0
+
+    while planted < @num_bombs
+      pos = Array.new(2) { rand(@grid_size) }
+      tile = self[pos]
+      next if tile.bombed?
+
+      tile.plant_bomb
+      planted +=  1
+    end
+
+    nil
   end
 end
